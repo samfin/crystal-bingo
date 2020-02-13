1026a3fd6feaf00d459d2d10fd45c255b2cfad9e
Geoffrey "TG" McCullough <50706444+HeyImTG@users.noreply.github.com>
2020-02-13 17:03:49 -0500

    Initial upload.
    Here it is!

---
+++ js/goal-list.js
@@ -0,0 +1,201 @@
+var bingoList = [];
+bingoList["info"] = {version: "InDev"};
+
+bingoList[1] = [
+	{ name: "Swallow An Enemy That Would Give You UFO", types: [ "ufo" ], subtypes: [ "stars", "gobbler" ] },
+	{ name: "Mustard Mountain Map", types: [ "mustard", "mustardmap" ], subtypes: [ "ufo" ] },
+	{ name: "Ride The Moonlight Mansion Overworld Warp Star", types: [ "stars", "moonlight" ], subtypes: [ "candy", "bonkers", "boxy" ] },
+	{ name: "Cabbage Cavern Map", types: [ "cabbage", "cabbagemap" ], subtypes: [ "music", "cabbagegoals" ] },
+	{ name: "Activate The Mustard Mountain Switch", types: [ "mustard", "northswitches" ], subtypes: [ "bombar", "kracko" ] }
+];
+
+bingoList[2] = [
+	{ name: "Activate Both Peppermint Palace Big Switches", types: [ "peppermint", "westswitches" ], subtypes: [ "ufo", "music", "sprays" ] },
+	{ name: "Both Moonlight Mansion Spray Cans", types: [ "sprays", "moonlight" ], subtypes: [ "moonlightgoals", "boxy", "batafire" ] },
+	{ name: "Use The Olive Ocean Cannon", types: [ "cannons", "olive" ], subtypes: [ "sprays", "olivegoals", "boxy" ] },
+	{ name: "Use The Mustard Mountain Cannon", types: [ "cannons", "mustard" ], subtypes: [ "kracko" ] },
+	{ name: "Radish Ruins Vitality", types: [ "radishvitality", "radish" ], subtypes: [ "metaknight", "hands", "moley", "music" ] }
+];
+
+bingoList[3] = [
+	{ name: "Kracko's Mirror Shard", types: [ "kracko", "mustard" ], subtypes: [ "batafire" ] },
+	{ name: "Ride The Mustard Mountain Overworld Warp Star", types: [ "stars", "mustard", "ufo" ], subtypes: [ "ufo", "candy", "boxer" ] },
+	{ name: "5 Maps", types: [ "maps", "mustardmap", "cabbagemap", "peppermintmap", "carrotmap", "olivemap", "radishmap", "candymap", "everywhere" ], subtypes: [ "switches", "goals" ] },
+	{ name: "Both Cabbage Cavern Music Sheets", types: [ "music", "cabbage" ], subtypes: [ "moley", "bonkers", "cabbagemap" ] },
+	{ name: "Enter Both Moonlight Mansion Goal Mirrors", types: [ "moonlightgoals", "moonlight" ], subtypes: [ "boxy", "batafire", "sprays" ] }
+];
+
+bingoList[4] = [
+	{ name: "Defeat 2 Different Boxys", types: [ "boxy" ], subtypes: [ "mustardgoals", "moonlightgoals", "radishgoals", "cannons" ] },
+	{ name: "Activate All 4 Rainbow Route Big Switches", types: [ "northswitches", "westswitches", "southswitches", "rainbow" ], subtypes: [ "cannons" ] },
+	{ name: "Activate All 3 Cabbage Cavern Big Switches", types: [ "southswitches", "cabbage" ], subtypes: [ "batafire", "bonkers", "moley", "sprays" ] },
+	{ name: "Activate The Candy Constellation Big Switch", types: [ "candyswitches", "candy" ], subtypes: [ "stars", "candyvitality" ] },
+	{ name: "Peppermint Palace Map", types: [ "peppermint", "peppermintmap" ], subtypes: [ "ufo", "westswitches", "sprays", "stars", "bonkers" ] }
+];
+
+bingoList[5] = [
+	{ name: "Activate The Olive Ocean Big Switch", types: [ "southswitches", "olive" ], subtypes: [ "cannons", "olivevitality", "mopassage", "frosty" ] },
+	{ name: "Moley's Mirror Shard", types: [ "cabbage", "moley" ], subtypes: [ "bonkers", "radish" ] },
+	{ name: "Carrot Castle Vitality", types: [ "carrotvitality", "carrot" ], subtypes: [ "megatitan", "phanphan" ] },
+	{ name: "Defeat 2 Different Mr. Frosties", types: [ "frosty" ], subtypes: [ "olive", "candy", "peppermint", "stars", "mopassage" ] },
+	{ name: "Sound Player", types: [ "candy" ], subtypes: [ "stars", "candyvitality", "sprays" ] },	
+];
+
+bingoList[6] = [
+	{ name: "Emerald (Radish Ruins Spray)", types: [ "radish", "sprays" ], subtypes: [ "radishgoals", "cannons", "metaknight" ] },
+	{ name: "Ride The Peppermint Palace Overworld Warp Star", types: [ "peppermint", "stars", "frosty" ], subtypes: [ "candy", "sprays", "westswitches", "candyswitches" ] },
+	{ name: "Defeat The Radish Ruins Bonkers", types: [ "bonkers", "radish" ], subtypes: [ "radishvitality", "cannons", "metaknight" ] },
+	{ name: "Ride The Carrot Castle Overworld Warp Star", types: [ "carrot", "stars" ], subtypes: [ "peppermint", "westswitches" ] },
+	{ name: "Defeat 2 Different Batafires", types: [ "batafire" ], subtypes: [ "moonlightgoals", "rainbowgoals", "kracko" ] }
+];
+
+bingoList[7] = [
+	{ name: "Defeat The Peppermint Palace Bombar", types: [ "peppermint", "bombar", "ufo" ], subtypes: [ "stars", "westswitches" ] },
+	{ name: "Olive Ocean Vitality", types: [ "olive", "olivevitality" ], subtypes: [ "gobbler", "frosty", "mopassage" ] },
+	{ name: "Carrot Castle Map", types: [ "carrot", "carrotmap" ], subtypes: [ "megatitan" ] },
+	{ name: "Use The Radish Ruins Cannon", types: [ "radish", "radishmap", "cannons" ], subtypes: [ "boxy", "radishgoals" ] },
+	{ name: "Candy Constellation Vitality", types: [ "candy", "candyvitality", "candyswitches" ], subtypes: [ "phanphan", "stars" ] }
+];
+
+bingoList[8] = [
+	{ name: "Both Peppermint Palace Music Sheets", types: [ "peppermint", "music" ], subtypes: [ "westswitches", "stars" ] },
+	{ name: "Radish Ruins Map", types: [ "radish", "radishmap" ], subtypes: [ "cannons", "boxy" ] },
+	{ name: "3 Music Sheets", types: [ "music" ], subtypes: [ "cabbage", "metaknight", "peppermint" ] },
+	{ name: "Activate 8 Big Switches", types: [ "switches", "northswitches", "southswitches", "westswitches", "candyswitches", "everywhere" ], subtypes: [ "maps", "bosses", "miniboss" ] },
+	{ name: "Mega Titan's Mirror Shard", types: [ "carrot", "megatitan", "boses" ], subtypes: [ "crpassage", "carrotvitality", "phanphan", "carrotmap" ] }
+];
+
+bingoList[9] = [
+	{ name: "3 Spray Cans", types: [ "sprays" ], subtypes: [ "cabbage", "peppermint", "bonkers" ] },
+	{ name: "Defeat 3 Different Boxys", types: [ "boxy" ], subtypes: [ "mustardgoals", "moonlightgoals", "radishgoals", "cannons" ] },
+	{ name: "Candy Constellation Map", types: [ "candy", "candymap" ], subtypes: [ "stars", "bonkers" ] },
+	{ name: "Defeat Each Miniboss In Moonlight Mansion Once", types: [ "moonlight" ], subtypes: [ "boxy", "bonkers", "sprays", "boxer", "batafire", "moonlightgoals" ] },
+	{ name: "All 5 Mustard Mountain Chests", types: [ "mustard", "mustardmap" ], subtypes: [ "kracko", "batafire", "ufo" ] }
+];
+
+bingoList[10] = [
+	{ name: "Defeat 2 Different Bonkers", types: [ "bonkers" ], subtypes: [ "cabbage", "olivemap", "peppermintmap", "candymap", "moley", "gobbler" ] },
+	{ name: "All 7 Moonlight Mansion Chests", types: [ "moonlight", "sprays" ], subtypes: [ "mopassage", "stars", "candy", "boxy", "moonlightgoals" ] },
+	{ name: "Olive Ocean Map", types: [ "olivemap", "olive" ], subtypes: [ "bonkers", "gobbler" ] },
+	{ name: "Clear The Castle / Ruins Passage", types: [ "crpassage", "radish" ], subtypes: [ "radishvitality", "radishmap", "metaknight", "cannons", "westswitches", "southswitches" ] },
+	{ name: "Radish Ruins Vitality And Map", types: [ "radishvitality", "radish", "radishmap" ], subtypes: [ "cannons", "bonkers", "boxy", "metaknight", "crpassage" ] }
+];
+
+bingoList[11] = [
+	{ name: "8 Vitality", types: [ "radishvitality", "carrotvitality", "candyvitality", "olivevitality" ], subtypes: [ "radish", "carrot", "megatitan" ] },
+	{ name: "Wiz's Mirror Shard", types: [ "wiz", "peppermint" ], subtypes: [ "westswitches", "sprays" ] },
+	{ name: "Enter All 3 Rainbow Route Goal Mirrors", types: [ "rainbowgoals", "rainbow" ], subtypes: [ "batafire", "boxer", "hands", "westswitches" ] },
+	{ name: "Clear The Mansion / Ocean Passage", types: [ "mopassage", "olive" ], subtypes: [ "sprays", "moonlightgoals", "olivegoals", "moonlight" ] },
+	{ name: "6 Maps", types: [ "maps", "mustardmap", "cabbagemap", "peppermintmap", "carrotmap", "olivemap", "radishmap", "candymap", "everywhere" ], subtypes: [ "bosses", "goals", "cabbage", "switches" ] }
+];
+
+bingoList[12] = [
+	{ name: "Defeat 2 Different Hand Minibosses", types: [ "hands" ], subtypes: [ "radish", "candy", "stars", "metaknight", "crazy" ] },
+	{ name: "Both Cabbage Cavern Spray Cans", types: [ "cabbage", "sprays" ], subtypes: [ "moley", "bonkers", "batafire", "boxer" ] },
+	{ name: "Candy Constellation Vitality and Map", types: [ "candy", "candyvitality", "candymap" ], subtypes: [ "hands", "stars", "boxer", "bonkers" ] },
+	{ name: "Enter Both Olive Ocean Goal Mirrors", types: [ "olive", "olivegoals", "frosty" ], subtypes: [ "music", "mopassage", "sprays" ] },
+	{ name: "Enter Both Mustard Mountain Goal Mirrors", types: [ "mustardgoals", "mustard" ], subtypes: [ "boxy", "bombar", "sprays" ] }
+];
+
+bingoList[13] = [
+	{ name: "Enter Both Radish Ruins Goal Mirrors", types: [ "radish", "radishgoals" ], subtypes: [ "boxy", "radishvitality", "cannons", "radishmap", "sprays", "crpassage" ] },
+	{ name: "4 Music Sheets", types: [ "music", "everywhere" ], subtypes: [ "goals", "maps", "cabbage" ] },
+	{ name: "Use 2 Different Fused Cannons", types: [ "cannons" ], subtypes: [ "radish", "olive", "southswitches" ] },
+	{ name: "Gobbler's Mirror Shard", types: [ "olive", "gobbler" ], subtypes: [ "olivevitality", "olivemap" ] },
+	{ name: "All 3 Peppermint Palace Sprays", types: [ "peppermint", "sprays" ], subtypes: [ "wiz", "peppermintmap", "stars", "frosty", "bonkers", "westswitches" ] }
+];
+
+bingoList[14] = [
+	{ name: "??? (Dark Meta Knight)'s Mirror Shard", types: [ "metaknight", "radish" ], subtypes: [ "music", "hand", "radishvitality", "crpassage" ] },
+	{ name: "Defeat 2 Different Box Boxers", types: [ "boxer" ], subtypes: [ "rainbowgoals", "candygoals" ] },
+	{ name: "Kirby's Sounds (Candy Constellation Music Sheet)", types: [ "candy", "music" ], subtypes: [ "hands", "crazy", "ufo" ] },
+	{ name: "Enter Every Room In Cabbage Cavern", types: [ "cabbage" ], subtypes: [ "music", "sprays", "gates", "moley", "bosses", "batafire", "bonkers" ] },
+	{ name: "4 Spray Cans", types: [ "sprays" ], subtypes: [ "goals", "peppermint" ] }
+];
+
+bingoList[15] = [
+	{ name: "Olive Ocean Vitality and Map", types: [ "olivevitality", "olivemap", "olive" ], subtypes: [ "bonkers", "gobbler", "mopassage" ] },
+	{ name: "Defeat 2 Different Phan Phans", types: [ "phanphan" ], subtypes: [ "candyvitality", "carrotvitality" ] },
+	{ name: "Enter Every Room In Moonlight Mansion", types: [ "moonlight" ], subtypes: [ "stars", "moonlightgoals", "sprays", "batafire", "bonkers", "boxer", "boxy" ] },
+	{ name: "Defeat 3 Different Batafires", types: [ "batafire" ], subtypes: [ "rainbowgoals", "moonlightgoals", "kracko" ] },
+	{ name: "Both Moley and Kracko's Mirror Shards", types: [ "kracko", "moley" ], subtypes: [ "mustard", "cabbage", "bonkers", "batafire" ] }
+];
+
+bingoList[16] = [
+	{ name: "Enter Both Peppermint Palace Goal Mirrors", types: [ "peppermintgoals", "peppermint" ], subtypes: [ "westswitches", "music", "phanphan" ] },
+	{ name: "Defeat 3 Different Bonkers", types: [ "bonkers" ], subtypes: [ "cabbage", "radishmap", "sprays", "peppermintmap", "candymap", "moley", "gobbler" ] },
+	{ name: "Carrot Castle Vitality and Map", types: [ "carrotvitality", "vitality", "carrotmap" ], subtypes: [ "megatitan", "phanphan", "crpassage" ] },
+	{ name: "Activate 10 Big Switches", types: [ "everywhere", "northswitches", "southswitches", "westswitches", "candyswitches", "switches" ], subtypes: [ "bosses", "miniboss" ] },
+	{ name: "3 Mirror Shards", types: [ "moley", "kracko", "megatitan", "gobbler", "wiz", "metaknight", "crazy", "bosses" ], subtypes: [ "moley", "kracko", "megatitan", "southswitches", "olivemap", "carrotmap", "carrotvitality", "radishvitality", "olivevitality" ] }
+];
+
+bingoList[17] = [
+	{ name: "Enter Both Candy Constellation Goal Mirrors", types: [ "candygoals", "candy", "candymap" ], subtypes: [ "bonkers", "boxer", "hand", "crazy", "phanphan", "candyvitality" ] },
+	{ name: "Defeat Each Miniboss In Radish Ruins Once", types: [ "radish", "miniboss" ], subtypes: [ "cannons", "boxy", "bonkers", "hands", "metaknight", "crpassage" ] },
+	{ name: "Defeat 4 Different Boxys", types: [ "boxy" ], subtypes: [ "radishgoals", "mustardgoals", "moonlightgoals", "cannons" ] },
+	{ name: "Both Mega Titan and Wiz's Mirror Shards", types: [ "megatitan", "wiz" ], subtypes: [ "peppermint", "carrot", "westswitches", "carrotvitality" ] },
+	{ name: "All 6 Carrot Castle Chests", types: [ "carrot", "carrotmap", "carrotvitality" ], subtypes: [ "megatitan",  "crpassage" ] }
+];
+
+bingoList[18] = [
+	{ name: "Defeat 3 Different Phan Phans", types: [ "phanphan" ], subtypes: [ "carrotvitality", "candyvitality" ] },
+	{ name: "Enter Every Room In Carrot Castle", types: [ "carrot" ], subtypes: [ "carrotvitality", "megatitan" ] },
+	{ name: "7 Maps", types: [ "maps", "mustardmap", "cabbagemap", "peppermintmap", "carrotmap", "olivemap", "radishmap", "candymap", "everywhere" ], subtypes: [ "switches", "bonkers" ] },
+	{ name: "Enter 4 Different Goal Mirrors", types: [ "rainbowgoals", "moonlightgoals", "cabbagegoals", "mustardgoals", "olivegoals", "peppermintgoals", "radishgoals", "candygoals", "goals", "everywhere" ], subtypes: [ "miniboss", "sprays", "switches" ] }
+];
+
+bingoList[19] = [
+	{ name: "Defeat Each Miniboss In Cabbage Cavern Once", types: [ "miniboss", "cabbage", "bonkers" ], subtypes: [ "batafire", "boxer", "southswitches", "moley", "sprays" ] },
+	{ name: "Defeat Each Miniboss In Rainbow Route Once", types: [ "miniboss", "rainbow", "rainbowgoals" ], subtypes: [ "hands", "batafire", "boxer" ] },
+	{ name: "Defeat 4 Different Batafires", types: [ "batafire" ], subtypes: [ "rainbowgoals", "moonlightgoals", "kracko", "northswitches", "southswitches" ] },
+	{ name: "Defeat Each Miniboss In Peppermint Palace Once", types: [ "peppermint", "frosty" ], subtypes: [ "phanphan", "bombar", "boxer", "bonkers", "ufo", "stars", "candy" ] },
+	{ name: "Ride 3 Different Overworld Warp Stars", types: [ "stars", "frosty" ], subtypes: [ "peppermint", "candy" ] }
+];
+
+bingoList[20] = [
+	{ name: "Defeat 4 Different Bonkers", types: [ "bonkers" ], subtypes: [ "cabbage", "sprays", "radishmap", "candymap", "olivemap", "peppermintmap", "moley", "gobbler" ] },
+	{ name: "All 8 Cabbage Cavern Chests", types: [ "cabbage", "sprays", "music" ], subtypes: [ "bonkers", "moley" ] },
+	{ name: "Master Hand and Crazy Hand's Mirror Shard", types: [ "crazy", "candy" ], subtypes: [ "hands", "ufo", "sprays", "music" ] },
+	{ name: "Defeat 3 Different Box Boxers", types: [ "boxer" ], subtypes: [ "candygoals", "rainbowgoals", "stars" ] },
+	{ name: "Activate 12 Big Switches", types: [ "switches", "northswitches", "southswitches", "westswitches", "candyswitches", "everywhere" ], subtypes: [ "maps", "bosses" ] }
+];
+
+bingoList[21] = [
+	{ name: "6 Maps That Aren't Cabbage Cavern Or Mustard Mountain", types: [ "maps", "mustardmap", "cabbagemap", "peppermintmap", "carrotmap", "olivemap", "radishmap", "candymap", "everywhere" ], subtypes: [ "switches", "bonkers" ] },
+	{ name: "Enter Every Room In Rainbow Route", types: [ "rainbow" ], subtypes: [ "rainbowgoals", "hands", "boxer", "batafire" ] },
+	{ name: "Enter Every Room In Radish Ruins", types: [ "radish" ], subtypes: [ "radishgoals", "boxy", "metaknight", "cannons" ] },
+	{ name: "9 Vitality", types: [ "carrotvitality", "radishvitality", "candyvitality", "olivevitality" ], subtypes: [ "radish", "candy", "carrot", "megatitan", "southswitches", "westswitches", "candyswitches" ] },
+	{ name: "Enter Every Room In Peppermint Palace", types: [ "peppermint" ], subtypes: [ "phanphan", "sprays", "wiz", "bonkers", "boxer", "bombar", "stars", "music", "peppermintgoals" ] }
+];
+
+bingoList[22] = [
+	{ name: "Defeat 5 Different Bonkers", types: [ "bonkers" ], subtypes: [ "sprays", "cabbage", "olivemap", "candymap", "radishmap", "peppermintmap", "moley", "gobbler" ] },
+	{ name: "Enter Every Room In Mustard Mountain", types: [ "mustard" ], subtypes: [ "mustardgoals", "boxy", "kracko", "cannons", "batafire", "bombar" ] },
+	{ name: "All 7 Radish Ruins Chests", types: [ "radish", "radishmap", "radishvitality" ], subtypes: [ "metaknight", "radishgoals", "cannons", "crpassage" ] },
+	{ name: "Defeat 4 Different Box Boxers", types: [ "boxer" ], subtypes: [ "rainbowgoals", "candygoals", "stars" ] },
+	{ name: "Ride 4 Different Overworld Warp Stars", types: [ "stars", "frosty" ], subtypes: [ "peppermint", "candy" ] }
+];
+
+bingoList[23] = [
+	{ name: "Defeat Each Miniboss In Mustard Mountain Once", types: [ "bombar", "mustard" ], subtypes: [ "bombar", "phanphan", "boxer", "boxy", "mustardgoals" ] },
+	{ name: "All 9 Candy Constellation Chests", types: [ "candy", "sprays", "candymap", "candyvitality" ], subtypes: [ "candygoals", "phanphan", "hands", "crazy" ] },
+	{ name: "Both Gobbler and ??? (Dark Meta Knight)'s Mirror Shards", types: [ "gobbler", "metaknight" ], subtypes: [ "radish", "olive", "olivevitality", "radishvitality", "cannons" ] },
+	{ name: "Enter 5 Different Goal Mirrors", types: [ "rainbowgoals", "moonlightgoals", "cabbagegoals", "mustardgoals", "olivegoals", "peppermintgoals", "radishgoals", "candygoals", "goals", "everywhere" ], subtypes: [ "switches", "miniboss" ] },
+	{ name: "Enter Every Room In Olive Ocean", types: [ "olive" ], subtypes: [ "olivegoals", "frosty", "gobbler", "cannons", "boxy", "bonkers", "olivevitality" ] }
+];
+
+bingoList[24] = [
+	{ name: "8 Maps", types: [ "maps", "mustardmap", "cabbagemap", "peppermintmap", "carrotmap", "olivemap", "radishmap", "candymap", "everywhere" ], subtypes: [ "switches", "bonkers" ] },
+	{ name: "All 7 Olive Ocean Chests", types: [ "olive", "sprays", "music", "frosty", "olivemap", "olivevitality" ], subtypes: [ "gobbler", "mopassage" ] },
+	{ name: "4 Mirror Shards", types: [ "bosses", "moley", "kracko", "megatitan", "gobbler", "wiz", "metaknight", "crazy", "everywhere" ], subtypes: [ "kracko", "moley", "megatitan", "metaknight", "gobbler", "olivemap", "carrotmap", "radishvitality", "carrotvitality", "olivevitality" ] },
+	{ name: "Open 1 Small Chest From Each Area", types: [ "everywhere" ], subtypes: [ "switches" ] },
+	{ name: "Enter Every Room In Candy Constellation", types: [ "candy", "stars" ], subtypes: [ "candygoals", "candyvitality", "hands", "crazy", "sprays" ] }
+];
+
+bingoList[25] = [
+	{ name: "All 20 Peppermint Palace Chests", types: [ "peppermint", "smallchests", "ufo", "sprays" ], subtypes: [ "peppermintgoals", "phanphan", "bombar", "stars", "frosty" ] },
+	{ name: "Enter 6 Different Goal Mirrors", types: [ "rainbowgoals", "moonlightgoals", "cabbagegoals", "mustardgoals", "olivegoals", "peppermintgoals", "radishgoals", "candygoals", "goals", "everywhere" ], subtypes: [ "miniboss", "maps", "music" ] },
+	{ name: "5 Mirror Shards", types: [ "bosses", "moley", "kracko", "megatitan", "gobbler", "wiz", "metaknight", "crazy", "everywhere" ], subtypes: [ "kracko", "moley", "megatitan", "metaknight", "gobbler", "olivemap", "carrotmap", "carrotvitality", "radishvitality", "olivevitality" ] },
+	{ name: "All 10 Rainbow Route Chests", types: [ "rainbow" ], subtypes: [ "rainbowgoals", "boxer", "batafire", "hands" ] },
+	{ name: "10 Vitality", types: [ "carrotvitality", "radishvitality", "olivevitality", "candyvitality" ], subtypes: [ "radish", "olive", "candy", "carrot" ] }
+];
